31e3e6913d09df9450e4dff5a30e14f7b34149a3
snowball58 <stevensnow58@gmail.com>
2016-03-10 22:15:09 +0000

    Set up transition between pages.

--- webform/app/controllers/welcome_controller.rb
+++ webform/app/controllers/welcome_controller.rb
@@ -2,6 +2,10 @@ class WelcomeController < ApplicationController
   def index
     
   end
+  def indexCheck
+    # check input, other processing here
+    redirect_to welcome_volunteer_path
+  end
   def volunteer
     puts "This is the volunteer controller."
   end
--- webform/config/routes.rb
+++ webform/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   get 'welcome/volunteer', :to => 'welcome#volunteer'
+  post 'welcome/index', :to => 'welcome#indexCheck'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
