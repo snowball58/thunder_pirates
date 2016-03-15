aa79a32a06b4f13fe6e61f2a4af3d783657818ba
snowball58 <stevensnow58@gmail.com>
2016-03-15 22:30:07 +0000

    Finished first version of volunteer view.

--- webform/config/routes.rb
+++ webform/config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
   
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  resources :volunteer, :controller => "welcome"
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
