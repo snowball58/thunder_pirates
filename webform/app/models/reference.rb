80e8adbf6911865341d846f07344136c53512eb3
matthewwiecek <matthewwiecek@gmail.com>
2016-03-30 21:18:17 +0000

    Added the necessary data to create the database

--- webform/app/models/reference.rb
+++ webform/app/models/reference.rb
@@ -1,6 +1,5 @@
 class Reference < ActiveRecord::Base
 
-  attr_accessible :'Does this person usually exercise good judgment'
-  
+  attr_accessible :'Does this person usually exercise good judgment', :'Signature_4', :'Date_4', :'Print Name', :'Volunteer Name', :'How long have you known this person', :'Capable in crisis situation', :'Cable in crisis situation, why', :'Good judgment, elaborate', :'Do you have any hesitation about this person working in this capacity', :'Pertinent Information'
   
 end
--- webform/db/migrate/.20160330203012_reference.rb
+++ webform/db/migrate/.20160330203012_reference.rb
@@ -2,6 +2,16 @@ class Reference < ActiveRecord::Migration
   def up
     create_table :Reference do |t|
       t.string  :'Does this person usually exercise good judgment'
+      t.string	:'Signature_4'
+      t.string	:'Date_4'
+      t.string	:'Print Name'
+      t.string	:'Volunteer Name'
+      t.string	:'How long have you known this person'
+      t.string	:'Capable in crisis situation'
+      t.string	:'Cable in crisis situation, why'
+      t.string	:'Good judgment, elaborate'
+      t.string	:'Do you have any hesitation about this person working in this capacity'
+      t.string	:'Pertinent Information'
     end
   end
 
