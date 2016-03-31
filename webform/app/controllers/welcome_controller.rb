4427f98be08b7d26d34ee42a0bfa5d908162806f
snowball58 <stevensnow58@gmail.com>
2016-03-31 19:51:19 +0000

    Volunteer controller mostly done.

--- webform/app/controllers/welcome_controller.rb
+++ webform/app/controllers/welcome_controller.rb
@@ -22,17 +22,67 @@ class WelcomeController < ApplicationController
   
   def volunteer
     #convoluted example, but makes the point
-    b = Volunteer.new()
-    b.uniqueID = 'TestID'
-    b.save
-    a = Volunteer.find_by_uniqueID('TestID')
-    puts 'LOLOLOLO'
-    puts a == nil
-    puts a[:uniqueID]
+    if session[:uniqueID].blank? or not Volunteer.exists?(:uniqueID => session[:uniqueID])
+      b = Volunteer.new()
+      uniqueID = SecureRandom.base64
+      b.uniqueID = uniqueID
+      b.save
+      session[:uniqueID] = uniqueID
+    else
+      volunteer = Volunteer.find_by_uniqueID(session[:uniqueID])
+      # query the text fields like this
+      @name = volunteer[:Name]
+      @birth = volunteer[:DateofBirth]
+      @email = volunteer[:EmailAddress]
+      @phone = volunteer[:PhoneNumbers]
+      @address = volunteer[:MailingAddress]
+      @county = volunteer[:County]
+      @additional = volunteer[:IfyouhaveselectedAdditionalVolunteerOpportunitiespleasespecify]
+      @times = volunteer[:DaysTimesyouwillbeavailabletovolunteer]
+      
+      # query check boxes like this
+      # if box is saved as not being checked, should have empty value in database!
+      # if box is saved as being checked, should have "Yes" as value for pdf purposes
+      @family_care = volunteer[:FamilyCare]
+      @ambassador = volunteer[:Ambassador]
+      @virtual_volunteer = volunteer[:VirtualVolunteer]
+      @medical_volunteer = volunteer[:MedicalVolunteer]
+      @counseling_internship = volunteer[:ProgramCounselingInternship]
+      @outlying_county = volunteer[:OutlyingCountyAmbassadorProgram] 
+      @additionl_opportunities = volunteer[:AdditionalVolunteerOpportunities]
+    end
   end
   
   def volunteer_check
-    session[:test] = params[:name]
+    #some fields might be required later
+    volunteer = Volunteer.find_by_uniqueID(session[:uniqueID])
+    
+    #this is how you change text fields in the database
+    volunteer.Name = params[:name]
+    volunteer.DateofBirth = params[:birth]
+    volunteer.EmailAddress = params[:email]
+    volunteer.PhoneNumbers = params[:phone]
+    volunteer.MailingAddress = params[:address]
+    volunteer.County = params[:county]
+    volunteer.IfyouhaveselectedAdditionalVolunteerOpportunitiespleasespecify = params[:additional]
+    volunteer.DaysTimesyouwillbeavailabletovolunteer = params[:times]
+    
+    #this is where I fill in the date of the application
+    time = Time.new
+    volunteer.DateofApplication = time.month.to_s + "/" + time.day.to_s + "/" + time.year.to_s
+    puts volunteer.DateofApplication
+    
+    #this is how to change check boxes in the database
+    volunteer.FamilyCare = params[:family]
+    volunteer.Ambassador = params[:ambassador]
+    volunteer.VirtualVolunteer = params[:virtual]
+    volunteer.MedicalVolunteer = params[:medical]
+    volunteer.ProgramCounselingInternship = params[:counseling]
+    volunteer.OutlyingCountyAmbassadorProgram = params[:outlying]
+    volunteer.AdditionalVolunteerOpportunities = params[:additional]
+    
+    # save the changes made to the volunteer entry
+    volunteer.save
     redirect_to welcome_general_info_path
   end
   
