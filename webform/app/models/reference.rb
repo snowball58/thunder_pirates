16d908dfc5b01028675dbdb6248feec9beb48b12
matthewwiecek <matthewwiecek@gmail.com>
2016-04-04 19:52:13 +0000

    Commented out unnecessary attr_accessor

--- webform/app/models/reference.rb
+++ webform/app/models/reference.rb
@@ -1,5 +1,5 @@
 class Reference < ActiveRecord::Base
 
-  attr_accessor :uniqueId, :VolunteerId, :Signature_4, :Date_4, :VolunteerName, :Howlonghaveyouknownthisperson, :Capableincrisissituation, :Cableincrisissituationwhy, :Doesthispersonusuallyexercisegoodjudgment, :Goodjudgmentelaborate, :Doyouhaveanyhesitationaboutthispersonworkinginthiscapacity, :PertinentInformation, :ReferenceName
+  #attr_accessor :uniqueId, :VolunteerId, :Signature_4, :Date_4, :VolunteerName, :Howlonghaveyouknownthisperson, :Capableincrisissituation, :Cableincrisissituationwhy, :Doesthispersonusuallyexercisegoodjudgment, :Goodjudgmentelaborate, :Doyouhaveanyhesitationaboutthispersonworkinginthiscapacity, :PertinentInformation, :ReferenceName
   
 end
