aa4f8b853f74393267cfb0e448c5d4ca7affc487
matthewwiecek <matthewwiecek@gmail.com>
2016-04-10 02:04:48 +0000

    Modified to ensure tests actually run

--- webform/spec/record_cleanup_spec.rb
+++ webform/spec/record_cleanup_spec.rb
@@ -1,6 +1,8 @@
-require_relative '../tasks/db.rake'
 require_relative 'rails_helper'
 require 'rake'
+load File.expand_path("../../lib/tasks/db.rake", __FILE__)
+# make sure you set correct relative path 
+Rake::Task.define_task(:environment)
 #require 'byebug'
 
 describe 'rake Task' do
@@ -9,23 +11,29 @@ describe 'rake Task' do
     let!(:recent_incomplete_record) { Volunteer.create(:date_modified => Time.now) }
     let!(:recent_complete_record) { Volunteer.create(:date_modified => Time.now, :EmergencyName => 'dude') }
     let!(:old_incomplete_reference) { Reference.create(:date_modified => Time.now - (60*60*25)) }
-    let!(:old_complete_reference) { Reference.create(:date_modified => Time.now - (60*60*24*31), Howlonghaveyouknownthisperson => '12')}
+    let!(:old_complete_reference) { Reference.create(:date_modified => Time.now - (60*60*24*31), :Howlonghaveyouknownthisperson => '12')}
     let!(:recent_incomplete_reference) { Reference.create(:date_modified => Time.now) }
-    let!(:recent_complete_reference) { Reference.create(:date_modified => Time.now, Howlonghaveyouknownthisperson => '12') }
+    let!(:recent_complete_reference) { Reference.create(:date_modified => Time.now, "Howlonghaveyouknownthisperson => '12') }
     let!(:blank_record) { Volunteer.create() }
     let!(:blank_ref) { Reference.create() }
     
-    describe 'add timestamp to blank documents' do
-        Rake::Task[db:assignDate].invoke
-        
+    it 'add timestamp to blank documents' do
+        #Rake::Task['db:assignDate'].invoke
+        Volunteer.find_each do |vol|
+            vol.date_modified.should_not be_nil
+            puts vol
+        end
+        Reference.find_each do |vol|
+            vol.date_modified.should_not be_nil
+        end
     end
 
-    describe 'wipe old incomplete records' do
-        Rake::Task[db:shortEraseData].invoke
+    it 'wipe old incomplete records' do
+        Rake::Task['db:shortEraseData'].invoke
         
     end
-    describe 'wipe old complete records' do
-        Rake::Task[db:longEraseData].invoke
+    it 'wipe old complete records' do
+        Rake::Task['db:longEraseData'].invoke
         
     end
 end
