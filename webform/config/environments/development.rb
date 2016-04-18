35a2af0f7284082cbadee5ef7e1d6bb2b5c4ddfb
matthewwiecek <matthewwiecek@gmail.com>
2016-04-18 21:43:09 +0000

    Finally fixed config, server will run and dynamically fetch URL (still needs work, but runs)

--- webform/config/environments/development.rb
+++ webform/config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
     
   #environment variables don't play nice in cloud 9
-  config.domain = 'csce431-project-matthewwiecek.c9users.io'
+  config.domain = nil
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
