8189e41aad7d81b615ca4fe7c666b20d8e44c7b8
matthewwiecek <matthewwiecek@gmail.com>
2016-04-28 03:33:08 +0000

    Changed the way alerts and notices are handled, Admin side alerts work now, alerts in general are prettier

--- webform/app/controllers/welcome_controller.rb
+++ webform/app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 # Scotty's House Volunteer WebForm
-# Written in 2016 by:
+# Written in 2016 by: 
 # Gustavo Estrela
 # Logan Ford
 # Patrick Herrington
@@ -11,29 +11,29 @@
 
 class WelcomeController < ApplicationController
   layout 'welcome'
-
+  
   def index
     ActionMailer::Base.default_url_options = {:host => request.host_with_port}
   end
 
-  # This function finds the user in the database. If he's not, the user will be
+  # This function finds the user in the database. If he's not, the user will be 
   # redirected to the index
   def checkin_user
     volunteer = Volunteer.find_by_uniqueID(session[:uniqueID])
     if volunteer == nil
-      flash[:notice] = "We couldn't find your data, start the form from the beggining."
+      flash[:alert] = "We couldn't find your data, start the form from the beggining."
       redirect_to welcome_index_path and return
     end
     volunteer
   end
-
+  
   def index_check
     # check input, other processing here
     redirect_to welcome_volunteer_path # go to next if everything is good
   end
-
+  
   def volunteer
-    if params[:uniqueID]
+    if params[:uniqueID] 
       session[:uniqueID] = params[:uniqueID]
     end
     if not Volunteer.exists?(:uniqueID => session[:uniqueID])
@@ -60,7 +60,7 @@ class WelcomeController < ApplicationController
       @county = volunteer[:County]
       @additional = volunteer[:IfyouhaveselectedAdditionalVolunteerOpportunitiespleasespecify]
       @times = volunteer[:DaysTimesyouwillbeavailabletovolunteer]
-
+      
       # query check boxes like this
       # if box is saved as not being checked, should have empty value in database!
       # if box is saved as being checked, should have "Yes" as value for pdf purposes
@@ -69,19 +69,19 @@ class WelcomeController < ApplicationController
       @virtual_volunteer = volunteer[:VirtualVolunteer]
       @medical_volunteer = volunteer[:MedicalVolunteer]
       @counseling_internship = volunteer[:ProgramCounselingInternship]
-      @outlying_county = volunteer[:OutlyingCountyAmbassadorProgram]
+      @outlying_county = volunteer[:OutlyingCountyAmbassadorProgram] 
       @additionl_opportunities = volunteer[:AdditionalVolunteerOpportunities]
     end
   end
-
+  
   def volunteer_check
-
+    
     #some fields might be required later
     volunteer = Volunteer.find_by_uniqueID(session[:uniqueID])
-
+    
     #set timestamp modified
     volunteer.date_modified = Time.now
-
+    
     #this is how you change text fields in the database
     volunteer.Name = params[:name]
     volunteer.DateofBirth = params[:birth]
@@ -104,13 +104,13 @@ class WelcomeController < ApplicationController
     volunteer.County = params[:county]
     volunteer.IfyouhaveselectedAdditionalVolunteerOpportunitiespleasespecify = params[:additional]
     volunteer.DaysTimesyouwillbeavailabletovolunteer = params[:times]
-
+    
     #this is where I fill in the date of the application
     time = Time.new
     #volunteer.date_modified = DateTime.new
     volunteer.DateofApplication = time.month.to_s + "/" + time.day.to_s + "/" + time.year.to_s
     #puts volunteer.DateofApplication
-
+    
     #this is how to change check boxes in the database
     volunteer.FamilyCare = params[:family]
     volunteer.Ambassador = params[:ambassador]
@@ -119,101 +119,101 @@ class WelcomeController < ApplicationController
     volunteer.ProgramCounselingInternship = params[:counseling]
     volunteer.OutlyingCountyAmbassadorProgram = params[:outlying]
     volunteer.AdditionalVolunteerOpportunities = params[:additional2]
-
+    
     #save before checking to save other input from user
     volunteer.save
-
-    flash[:notice] = nil;
+    
+    flash[:alert] = nil;
     # check for required fields before moving on to the next page
     if params[:name].blank?
-      flash[:notice] = "Name field required."
+      flash[:alert] = "Name field required."
       redirect_to welcome_volunteer_path
       return
     end
     if params[:birth].blank?
-      flash[:notice] = "Date of Birth field required."
+      flash[:alert] = "Date of Birth field required."
       redirect_to welcome_volunteer_path
       return
     end
     if not EmailValidator.valid?(params[:email])
-      flash[:notice] = "Valid email required."
+      flash[:alert] = "Valid email required."
       redirect_to welcome_volunteer_path
       return
     end
     if params[:street].blank?
-      flash[:notice] = "Street Address / Appartment Number field required."
+      flash[:alert] = "Street Address / Appartment Number field required."
       redirect_to welcome_volunteer_path
       return
     end
     if params[:city].blank?
-      flash[:notice] = "City field required."
+      flash[:alert] = "City field required."
       redirect_to welcome_volunteer_path
       return
     end
     if params[:state].blank?
-      flash[:notice] = "State field required."
+      flash[:alert] = "State field required."
       redirect_to welcome_volunteer_path
       return
     end
     if params[:zip].blank?
-      flash[:notice] = "Zip Code field required."
+      flash[:alert] = "Zip Code field required."
       redirect_to welcome_volunteer_path
       return
     end
     if params[:home_phone].blank? and params[:cell_phone].blank?
-      flash[:notice] = "At least one phone number required."
+      flash[:alert] = "At least one phone number required."
       redirect_to welcome_volunteer_path
       return
     end
 
     redirect_to welcome_general_info_path
   end
-
+  
   def general_info
     # Check that the current user has an ID set on first page
     volunteer = checkin_user
     return if !volunteer
-
+    
     @program_source = volunteer[:HowdidyoulearnaboutScottysHouseandourVolunteerProgram]
     @experience_gain = volunteer[:Whatwouldyouliketogainfromyourvolunteerexperience]
     @reason_class_credit = volunteer[:ClassCredit]
-    @instructor_name = volunteer[:NameofInstructor]
+    @instructor_name = volunteer[:NameofInstructor]    
     @reason_org_credit = volunteer[:OrganizationCredit]
-    @organization_name = volunteer[:NameofOrganization]
+    @organization_name = volunteer[:NameofOrganization]              
     @reason_other = volunteer[:Other]
-    @explanation = volunteer[:OtherPleaseexplain]
+    @explanation = volunteer[:OtherPleaseexplain]              
     @employed = volunteer[:CurrentlyEmployed]
     @employer_name = volunteer[:NameofEmployer]
-
+  
   end
-
+  
   def general_info_check
     # Check that the current user has an ID set on first page
     volunteer = checkin_user
     return if !volunteer
-
+    
     volunteer.date_modified = Time.now
     volunteer.HowdidyoulearnaboutScottysHouseandourVolunteerProgram = params[:program_source]
     volunteer.Whatwouldyouliketogainfromyourvolunteerexperience   = params[:experience_gain]
-
+    
     reason = params[:reason]
     if reason == "class_credit" then
       volunteer.ClassCredit = "Yes"
-      volunteer.OrganizationCredit = "No"
+      volunteer.OrganizationCredit = "No"      
       volunteer.Other = "No"
       volunteer.NameofInstructor = params[:instructor_name]
     elsif reason == "org_credit" then
       volunteer.ClassCredit = "No"
-      volunteer.OrganizationCredit = "Yes"
+      volunteer.OrganizationCredit = "Yes"      
       volunteer.Other = "No"
       volunteer.NameofOrganization = params[:organization_name]
     elsif reason == "other" then
       volunteer.ClassCredit = "No"
-      volunteer.OrganizationCredit = "No"
+      volunteer.OrganizationCredit = "No"      
       volunteer.Other = "Yes"
       volunteer.OtherPleaseexplain = params[:explanation]
     end
-
+          
     employment = params[:employed]
     if employment == "yes" then
       volunteer.CurrentlyEmployed = "Yes"
@@ -316,7 +316,7 @@ class WelcomeController < ApplicationController
   end
 
   def skills
-
+    
     volunteer = checkin_user
     return if !volunteer
 
@@ -337,28 +337,28 @@ class WelcomeController < ApplicationController
       volunteer.SpeakotherlanguageYES = params[:multilingual_speaker]
       if volunteer.SpeakotherlanguageYES == "Yes"
         volunteer.SpeakotherlanguageNO = "no"
-      else
+      else 
         volunteer.SpeakotherlanguageNO = "Yes"
       end
       volunteer.Speaklanguage = params[:speaking_languages]
       volunteer.LiterateotherlanguageYES = params[:multilingual_reader]
       if volunteer.LiterateotherlanguageYES == "Yes"
         volunteer.LiterateotherlanguageNO = "no"
-      else
+      else 
         volunteer.LiterateotherlanguageNO = "Yes"
       end
       volunteer.Literatelanguage = params[:reading_languages]
       volunteer.ExperiencewithblinddeafpersonsYES = params[:impaired_experience]
       if volunteer.ExperiencewithblinddeafpersonsYES == "Yes"
         volunteer.ExperiencewithblinddeafpersonsNO = "no"
-      else
+      else 
         volunteer.ExperiencewithblinddeafpersonsNO = "Yes"
       end
       volunteer.Experiencewithblinddeafpersons = params[:impaired_capacity]
       volunteer.ExperiencewithhandicappersonsYES = params[:handicapped_experience]
-      if volunteer.ExperiencewithhandicappersonsYES == "Yes"
+      if volunteer.ExperiencewithhandicappersonsYES == "Yes" 
         volunteer.ExperiencewithhandicappersonsNO = "no"
-      else
+      else 
         volunteer.ExperiencewithhandicappersonsNO = "Yes"
       end
       volunteer.Experiencewithhandicappersons = params[:handicapped_capacity]
@@ -379,26 +379,26 @@ class WelcomeController < ApplicationController
   end
 
   def emergency_notification_check
-
+    
     volunteer = Volunteer.find_by_uniqueID(session[:uniqueID])
     return if !volunteer
     if params[:emergency_name].blank?
-      flash[:notice] = "Please enter a name for your emergency contact."
+      flash[:alert] = "Please enter a name for your emergency contact."
       redirect_to welcome_emergency_notification_path
       return
     end
     if params[:emergency_primary_phone].blank?
-      flash[:notice] = "Please enter a phone number for your emergency contact."
+      flash[:alert] = "Please enter a phone number for your emergency contact."
       redirect_to welcome_emergency_notification_path
       return
     end
     if params[:emergency_address].blank?
-      flash[:notice] = "Please enter an address for your emergency contact."
+      flash[:alert] = "Please enter an address for your emergency contact."
       redirect_to welcome_emergency_notification_path
       return
     end
     if params[:emergency_relationship].blank?
-      flash[:notice] = "Please enter your relationship with your emergency contact."
+      flash[:alert] = "Please enter your relationship with your emergency contact."
       redirect_to welcome_emergency_notification_path
       return
     end
@@ -408,14 +408,14 @@ class WelcomeController < ApplicationController
       volunteer.EmergencyRelationship = params[:emergency_relationship]
       volunteer.date_modified = Time.now
       volunteer.save
-
+    
     redirect_to welcome_reference_form_emails_path
   end
-
+  
   def reference_form_emails
     volunteer = checkin_user
     if !volunteer
-      return
+      return 
     end
     @root = root_url.to_s.chomp("/")
     @ref_id = session[:uniqueID]
@@ -423,37 +423,37 @@ class WelcomeController < ApplicationController
     @reference_email_2 = session[:reference_email_2]
     @reference_email_3 = session[:reference_email_3]
   end
-
+  
   def reference_form_emails_check
     volunteer = Volunteer.find_by_uniqueID(session[:uniqueID])
-
+    
     if volunteer != nil
       volunteer.date_modified = Time.now
       volunteer.save
-      flash[:notice] = nil
+      flash[:alert] = nil
       #if not (EmailValidator.valid?(params[:reference_email_1]) and EmailValidator.valid?(params[:reference_email_2]) and EmailValidator.valid?(params[:reference_email_3]))
       session[:reference_email_1] = params[:reference_email_1]
       session[:reference_email_2] = params[:reference_email_2]
       session[:reference_email_3] = params[:reference_email_3]
       if not (params[:reference_email_1].blank? or EmailValidator.valid?(params[:reference_email_1]))
-        flash[:notice] = "Invalid email for Reference 1. Please enter valid email or leave blank."
+        flash[:alert] = "Invalid email for Reference 1. Please enter valid email or leave blank."
         redirect_to welcome_reference_form_emails_path
         return
       end
       if not (params[:reference_email_2].blank? or EmailValidator.valid?(params[:reference_email_2]))
-        flash[:notice] = "Invalid email for Reference 2. Please enter valid email or leave blank."
+        flash[:alert] = "Invalid email for Reference 2. Please enter valid email or leave blank."
         redirect_to welcome_reference_form_emails_path
         return
       end
       if not (params[:reference_email_3].blank? or EmailValidator.valid?(params[:reference_email_3]))
-        flash[:notice] = "Invalid email for Reference 3. Please enter valid email or leave blank."
+        flash[:alert] = "Invalid email for Reference 3. Please enter valid email or leave blank."
         redirect_to welcome_reference_form_emails_path
         return
-      end
+      end 
     end
     redirect_to welcome_review_path
   end
-
+  
   def reference_form
     if params[:ref_id] != nil #&& session[:ref_unique_id] == nil or params[:ref_id] != nil && session[:ref_unique_id] != nil
       uID = SecureRandom.base64
@@ -479,7 +479,7 @@ class WelcomeController < ApplicationController
       #need an error page to redirect to
     end
   end
-
+  
   def reference_form_check
     reference = Reference.find_by_uniqueID(session[:ref_unique_id])
     if !reference
@@ -494,42 +494,42 @@ class WelcomeController < ApplicationController
     reference.PertinentInformation = params[:reference_form_area_5]
     reference.date_modified = Time.now
     reference.save
-
-    flash[:notice] = nil;
+    
+    flash[:alert] = nil;
     # check for required fields before moving on to the next page
     if params[:your_name].blank?
-      flash[:notice] = "Your name field required."
+      flash[:alert] = "Your name field required."
       redirect_to welcome_reference_form_path
       return
     end
     if params[:reference_name].blank?
-      flash[:notice] = "Volunteer name field required."
+      flash[:alert] = "Volunteer name field required."
       redirect_to welcome_reference_form_path
       return
     end
     if params[:reference_form_area_1].blank?
-      flash[:notice] = "Must answer: \"How long have you known this person?\""
+      flash[:alert] = "Must answer: \"How long have you known this person?\""
       redirect_to welcome_reference_form_path
       return
     end
     if params[:reference_form_area_2].blank?
-      flash[:notice] = "Must answer: \"Are they capable in a crisis situation?\""
+      flash[:alert] = "Must answer: \"Are they capable in a crisis situation?\""
       redirect_to welcome_reference_form_path
       return
     end
     if params[:reference_form_area_3].blank?
-      flash[:notice] = "Must answer: \"Does this person usually exercise good judgement?\""
+      flash[:alert] = "Must answer: \"Does this person usually exercise good judgement?\""
       redirect_to welcome_reference_form_path
       return
     end
     if params[:reference_form_area_4].blank?
-      flash[:notice] = "Must answer: \"Do you have any hesitations about this persons capacity?\""
+      flash[:alert] = "Must answer: \"Do you have any hesitations about this persons capacity?\""
       redirect_to welcome_reference_form_path
       return
     end
-
+    
     reference.save
-
+      
     ######### This part to be moved to confirmation page, if one is made for reference
     puts Reference.where('"VolunteerId" = ? and "ReferenceName" IS NOT NULL', reference.VolunteerId).count
     if Reference.where('"VolunteerId" = ? and "ReferenceName" IS NOT NULL', reference.VolunteerId).count >= 3
@@ -551,8 +551,8 @@ class WelcomeController < ApplicationController
       end
     end
     ######### This part to be moved to confirmation page
-
-
+    
+  
     redirect_to 'https://www.scottyshouse.org/'
   end
 
@@ -570,25 +570,23 @@ class WelcomeController < ApplicationController
     @state = volunteer[:State]
     @zip = volunteer[:Zip]
     @times = volunteer[:DaysTimesyouwillbeavailabletovolunteer]
-
+    
     #General Information
     @program_source = volunteer[:HowdidyoulearnaboutScottysHouseandourVolunteerProgram]
     @experience_gain = volunteer[:Whatwouldyouliketogainfromyourvolunteerexperience]
-
+    
     #Emergency Notification Information
     @emergency_name = volunteer[:EmergencyName]
     @emergency_primary_phone = volunteer[:EmergencyPhone]
     @emergency_address = volunteer[:EmergencyAddress]
     @emergency_relationship = volunteer[:EmergencyRelationship]
-
+    
     #Reference Emails
     @reference_email_1 = session[:reference_email_1]
     @reference_email_2 = session[:reference_email_2]
     @reference_email_3 = session[:reference_email_3]
-    
-    flash[:review] = true
   end
-
+  
   def review_check
      volunteer = Volunteer.find_by_uniqueID(session[:uniqueID])
     if volunteer != nil
@@ -616,7 +614,7 @@ class WelcomeController < ApplicationController
     end
     redirect_to welcome_thank_you_path
   end
-
+  
   def thank_you
     @root = root_url.to_s.chomp("/")
     @ref_id = session[:uniqueID]
@@ -624,7 +622,7 @@ class WelcomeController < ApplicationController
     return if !volunteer
     @name = volunteer[:Name]
   end
-
+  
   def status
     if params[:vol_id] != nil
       session[:uniqueID] = params[:vol_id]
@@ -645,7 +643,7 @@ class WelcomeController < ApplicationController
           @ref3 = r.ReferenceName
         end
       end
-
+      
       volunteer = Volunteer.find_by_uniqueID(params[:vol_id])
       if !volunteer
         #redirect to error
@@ -658,7 +656,7 @@ class WelcomeController < ApplicationController
       #need an error page to redirect to
     end
   end
-
+  
   def status_check
     redirect_to 'https://www.scottyshouse.org/'
   end
@@ -673,7 +671,7 @@ class WelcomeController < ApplicationController
     record = Volunteer.find_by_uniqueID(session[:uniqueID])
     send_file ScottyPDF.new(record).export('/tmp/application.pdf'), type: 'application/pdf' , :disposition => 'inline', :stream => false
   end
-
+  
   def refpdf
      record = Reference.find_by_uniqueID(session[:ref_unique_id])
      send_file RefPDF.new(record).export('/tmp/Ref.pdf'), type: 'application/pdf' , :disposition => 'inline', :stream => false
